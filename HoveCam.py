c85f9521ea34b0054c2242912cb8e20dc520bc85
Chris Murphy <chrismurphy01@gmail.com>
2019-05-15 22:52:17 +0100

    init

---
+++ HoveCam.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+import sys
+from twython import Twython
+
+CONSUMER_KEY = ''
+CONSUMER_SECRET = ''
+ACCESS_KEY = ''
+ACCESS_SECRET = ''
+
+api = Twyton(CONSUMER_KEY, CONSUMER_SECRET,ACCESS_KEY,ACCESS_SECRET)
+
+api.update_status(status=sys.argv[1])
