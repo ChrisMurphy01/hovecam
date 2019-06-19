6891c4735ef4fe8e47b38f5394525f62b3479151
Chris Murphy <chrismurphy01@gmail.com>
2019-06-19 16:41:09 +0100

    swapped out camera software to enable pause while it adjust to the light

--- HoveCam.py
+++ HoveCam.py
@@ -1,11 +1,19 @@
-#!/usr/bin/env python
-import os
+ #!/usr/bin/env python
+from picamera import PiCamera
 from twython import Twython
 from config import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET
+import time 
 
-api = Twython(CONSUMER_KEY, CONSUMER_SECRET,ACCESS_KEY,ACCESS_SECRET)
+api = Twython(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET)
 
-cmd = '/opt/vc/bin/vcgencmd measure_temp'
-line = os.popen(cmd).readline().strip()
-temp = line.split('=')[1].split("'")[0]
-api.update_status(status='CPU temp is '+temp+'C')
+camera = PiCamera()
+#camera.resolution = (1280, 780)
+camera.zoom = (0.25, 0.2, 0.5, 0.5)
+#camera.start_preview()
+time.sleep(5)
+camera.capture("webcam.jpg")
+#camera.stop_preview()
+
+photo = open('webcam.jpg', 'rb')
+
+api.update_status_with_media(media=photo, status='')
